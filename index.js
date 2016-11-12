9eaf66a211e7b48915d6451d96606fb4754b8075
Ian De La Cruz <ridlc10@gmail.com>
2016-11-12 11:21:05 +0800

    Serving files trial

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const fs = require('fs');
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = '8129';
 
 const server = http.createServer(function serverCallback (req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  fs.readFile('index.html', function serveFile(error, content) {
+    if(!error) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(content, 'utf-8');
+    }
+  })
 });
 
 server.listen(port, hostname, function listenCallback() {
