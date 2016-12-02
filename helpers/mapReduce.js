04121d07b4e857f14da912eca2cb22423e9cde8b
Ian De La Cruz <ridlc10@gmail.com>
2016-12-02 08:38:27 +0800

    Add helper module for mapReduce functions

---
+++ helpers/mapReduce.js
@@ -0,0 +1,16 @@
+ /**
+  * MapReduce helper functions
+  * @module helpers/mapReduce
+  *
+  */
+
+const mapReactions = function() {
+}
+
+const reduceReactions = function(keyObject, valuesArr) {
+}
+
+module.exports = {
+  mapReactions: mapReactions,
+  reduceReactions: reduceReactions
+}
